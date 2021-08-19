9bc7c176de0cfa6758afe0abba6b9a27b90b1eeb
Winnie Kang <o922887715@gmail.com>
2021-08-19 15:48:00 +0800

    Initial commit

---
+++ src/main/java/com/winniekang/Person.java
@@ -0,0 +1,7 @@
+package com.winniekang;
+
+public class Person {
+    public void hello() {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/kotlin/com/winniekang/Hello.kt
@@ -0,0 +1,14 @@
+package com.winniekang
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin")
+//    Human().hello()
+        val h = Human()
+        h.hello()
+}
+
+class Human {
+    fun hello() {
+        println("Hello kotlin")
+    }
+}
